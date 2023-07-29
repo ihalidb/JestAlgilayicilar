aec0bd8d69fd1eb37af283434b213e3d78b5bfac
İbrhaim Halid Bayrak <ibrahimh.bayrak@gmail.com>
2023-07-29 19:38:36 +0300

    tıklama ile dğeiştirilme yapıldı. boolean kullanıldı tren and false

--- JestAlgilayicilar/ViewController.swift
+++ JestAlgilayicilar/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    var ny = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(gorselDegistir))
+        
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
     }
-
+    
+    @objc func gorselDegistir(){
+        
+        if ny == false{
+            
+            label.text = "New York"
+            imageView.image = UIImage(named: "ny")
+            ny = true
+        } else {
+            
+            label.text = "San Francisco"
+            imageView.image = UIImage(named: "sf")
+            ny = false
+        }
+        
+    }
 
 }
 
